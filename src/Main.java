c5e401b6e1bbebd70e14455ab8f5f09d0d270653
nikolai_gjncharov_1 <gnp1912@gmail.com>
2023-01-03 18:09:01 +0300

    gev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-
+    String folder = "D:/œ–¿ “» ¿/SKILLBOX";
+        File file = new File(folder);
+        System.out.println(getFolderSize(file));
     }
+    public static long getFolderSize(File folder)
+    {
+        if(folder.isFile()){
+            return folder.length();
+        }
+        File[] files= folder.listFiles();
+        long sum=0;
+        for (File file: files){
+            sum+= getFolderSize(file);
+        }
+        return sum;
+    }
+
 }
